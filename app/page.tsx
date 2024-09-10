4f6d6c69fdb9bc66ac5a1985622699f9ae9279a4
Leo Lu <lyuholu@gmail.com>
2024-09-10 18:44:48 -0400

    setup: install react-chartjs-2

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ export default function Home() {
   return (
     <>
       <main>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
+        <h1>Charts</h1>
+        <section>
+          <h2>Candlestick Chart</h2>
+        </section>
+        <section>
+          <h2>Line Chart </h2>
+        </section>
+        <section>
+          <h2>Bar Chart</h2>
+        </section>
+        <section>
+          <h2>Pie Chart</h2>
+        </section>
       </main>
     </>
   );
