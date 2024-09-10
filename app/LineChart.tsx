d9587044a05d3b5f5860f1def70fd1b5d23d1454
Leo Lu <lyuholu@gmail.com>
2024-09-10 19:35:53 -0400

    feat: add line chart

---
+++ app/LineChart.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const FAKE_DATA = {
+  labels: [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ],
+  datasets: [
+    {
+      label: 'Steps',
+      data: [3000, 5000, 4500, 6000, 8000, 7000, 9000],
+      borderColor: 'rgb(75,192,192)',
+    },
+  ],
+};
+
+export default function LineChart() {
+  const options = {
+    aspectRatio: 1.5,
+  };
+
+  return (
+    <>
+      <section className="w-1/3">
+        <Line options={options} data={FAKE_DATA} />
+      </section>
+    </>
+  );
+}
