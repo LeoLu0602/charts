f936c82a0e270eb1d617721755f6073f42ce0d71
Leo Lu <lyuholu@gmail.com>
2024-09-10 20:01:25 -0400

    feat: add pie chart

---
+++ app/PieChart.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, Tooltip, Legend, ArcElement } from 'chart.js';
+
+ChartJS.register(Tooltip, Legend, ArcElement);
+
+const FAKE_DATA = {
+  labels: ['Facebook', 'Instagram', 'Twitter', 'Youtube', 'LinkedIn'],
+  datasets: [
+    {
+      label: 'Time Spent',
+      data: [120, 60, 30, 90, 45],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export default function PieChart() {
+  const options = {
+    aspectRatio: 2,
+  };
+
+  return (
+    <section className="w-2/5 m-4 font-bold">
+      <Pie options={options} data={FAKE_DATA} />
+    </section>
+  );
+}
